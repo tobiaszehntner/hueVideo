a793cdc30644460272d61ba37cacfcb7a8cea169
twobeers83 <tobi@tobiaszehntner.com>
2015-07-14 14:16:18 +0800

    adds video to screen

--- src/ofApp.h
+++ src/ofApp.h
@@ -18,5 +18,7 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+    ofVideoPlayer video;
 		
 };
